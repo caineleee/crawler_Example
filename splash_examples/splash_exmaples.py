bd736e7c6317ac810272d659076477179957463a
caineleee <dearfuhua@outlook.com>
2020-03-01 17:40:08 +0800

    update splash

--- splash_examples/splash_exmaples.py
+++ splash_examples/splash_exmaples.py
@@ -6,31 +6,31 @@ import requests
 
 
 # 获取浏览器渲染之后的源代码
-# splash_env = 'http://localhost:8050/render.html?url='
-# url = 'https:/www.baidu.com'
-# URL = splash_env + url
-#
-# resp = requests.get(URL)
-# print(resp.text)
+splash_env = 'http://localhost:8050/render.html?url='
+url = 'https:/www.baidu.com'
+URL = splash_env + url
+
+resp = requests.get(URL)
+print(resp.text)
 
 
 # 等待时长, 可以用于加载的等待时间设置
-# url = 'https://www.taobao.com'
-# wait = '&wait=5'
-# URL = splash_env + url + wait
-#
-# resp = requests.get(URL)
-# print(resp.text)
+url = 'https://www.taobao.com'
+wait = '&wait=5'
+URL = splash_env + url + wait
+
+resp = requests.get(URL)
+print(resp.text)
 
 
 # 以 Json 格式返回请求的信息
-# splash_env = 'http://localhost:8050/render.json?url='
-# url = 'https://www.taobao.com'
-# wait = '&wait=5'
-# URL = splash_env + url + wait
-#
-# resp = requests.get(URL)
-# print(resp.text)
+splash_env = 'http://localhost:8050/render.json?url='
+url = 'https://www.taobao.com'
+wait = '&wait=5'
+URL = splash_env + url + wait
+
+resp = requests.get(URL)
+print(resp.text)
 
 
 # 实现交互操作的神技能!!!!
